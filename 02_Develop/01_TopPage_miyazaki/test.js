d278ba81394406322a1a7ef4a5a5c832ba761f6f
宮崎滉平 <zaki@zakimiya.local>
2020-04-10 22:41:57 +0900

    status:コード修正、新規作成 comment:テスト用スクリプトの追加

---
+++ 02_Develop/01_TopPage_miyazaki/test.js
@@ -0,0 +1,27 @@
+//練習用スクリプト
+{
+    /*　個人的にこの書き方の方が好き
+    let startbtn = document.getElementById("btn-login");
+    startbtn.addEventListener("click", login_fuc);
+
+    function login_fuc(){
+        window.alert("ログインボタン押下");
+        document.getElementById("toppage-comment").textContent = "DOM操作";
+    };
+    */
+
+    /* 練習用 */
+    document.getElementById("btn-login").addEventListener("click", ()=>{
+        window.alert("ログインボタン押下");
+        document.getElementById("toppage-comment").textContent = "DOM操作";
+    });
+
+    /* ボタンを押したらイベント情報の文が増える */ 
+    document.getElementById("event-reload").addEventListener("click", ()=>{
+        let item = document.createElement("p");
+        item.textContent = "新しい記事です";
+
+        let event_info = document.querySelector("event-info");
+        event_info.appendChild(item);
+    });
+}
