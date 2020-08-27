e1c37899264611d9cbaa4a8a34e970b8c26d1451
Taishi Sato <lion.g.uc@gmail.com>
2020-08-27 20:58:29 +0900

    master

--- 02_Develop/02_Mypage/2.1_mypage/mypage.js
+++ 02_Develop/02_Mypage/2.1_mypage/mypage.js
@@ -1,6 +1,6 @@
 function onButtonClick() {		//ボタン押下
 	var tsubuyaki = document.getElementById("output");	//つぶやく入力の取得
-	if (tsubuyaki == null) {		//つぶやきに1文字以上ある場合の処理
+	if (tsubuyaki.value != null) {		//つぶやきに1文字以上ある場合の処理
 	    tsubuyaki.innerText = document.forms.id_form.id_textBox.value;		//テスト動作
 	}else{		//条件に合わない場合の処理
 		window.alert("1文字以上入力してください。");		//ウィンドウ警告
