bca919e78f638dd5ff0114dc6bb050d1781cc51d
宮崎滉平 <zaki@zakimiya.local>
2020-08-29 02:17:26 +0900

    修正

--- 02_Develop/js/index/index_func.js
+++ 02_Develop/js/index/index_func.js
@@ -1,16 +1,14 @@
 
   'use strict';
 {
-
-  /* index_func.html */
-  /* メールアドレスチェック関数 */
+  /* メールアドレスチェック */
   document.getElementById('loginbtn').addEventListener('click',()=>{
 
     /* メールアドレスチェック用の正規表現を格納 */
     let reg_mailAdress = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
     /* パスワードチェック用の正規表現を格納(半角英小文字大文字数字をそれぞれ1種類以上含む8文字以上15文字以下) */ 
     let reg_password = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,15}$/;
-    
+
     /* メールアドレス入力欄の値取得 */
     let mailAdress = document.getElementById('mailAdress').value;
     /* パスワード入力欄の値取得 */
